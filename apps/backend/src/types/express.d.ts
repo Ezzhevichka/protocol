a1a4446371caed8e6fc02e56df9d46d68365174f
AlexandrRCGIT <bitrix24.work@yandex.ru>
2026-05-16 21:35:46 +0700

    add public

--- apps/backend/src/types/express.d.ts
+++ apps/backend/src/types/express.d.ts
@@ -2,6 +2,7 @@ import type { User as UserType } from "@squad-admin/database";
 
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     interface User extends UserType {}
   }
 }
